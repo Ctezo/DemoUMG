e915936afaf9663aebc7ffc3c0e080cd96b2a11f
Carlos Tezo <ctezo@hotmail.com>
2022-05-21 02:07:16 -0600

    mejoras de cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp-CI-CD';
+  title = 'Bienvenidos al desarrollo Agil -CI-CD';
 }
